4f74e6127b7182ddb6d8e678496935921707f427
sthareecha <rs5227516@gmail.com>
2025-08-08 06:09:49 +0545

    Crud operation for student management

---
+++ Lab_7_EFCore/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Lab_7_EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace Lab_7_EFCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
