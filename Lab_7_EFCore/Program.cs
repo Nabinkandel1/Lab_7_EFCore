0701c97db0b8b58367869247d9ab22a668d5a5cf
yuyutshusharma <yuyutshusharma42@gmail.com>
2025-08-11 10:11:28 +0545

    Yo boy, its yuytshu here, doing work, yo!

--- Lab_7_EFCore/Program.cs
+++ Lab_7_EFCore/Program.cs
@@ -1,8 +1,15 @@
+using Lab_7_EFCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>
+    (
+    options => options.UseSqlServer(connectionString)
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
